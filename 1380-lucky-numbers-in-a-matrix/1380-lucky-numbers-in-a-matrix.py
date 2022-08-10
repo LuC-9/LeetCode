e8eb8ffe6932114cbfcbcc7795abe71ebf255248
aarsh <68937277+LuC-9@users.noreply.github.com>
2022-08-11 03:04:24 +0530

    Time: 129 ms (97.69%), Space: 14.1 MB (97.69%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.py
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.py
@@ -1,6 +1,6 @@
 class Solution:
     def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
-        minrow = {min(r) for r in matrix}
-        maxcol = {max(c) for c in zip(*matrix)}
-        print(list( c for c in zip(*matrix))) #oo lala
-        return list(minrow & maxcol)
+        minr = {min(r)for r in matrix}
+        maxc = {max(c)for c in zip(*matrix)}
+        #print(list( c for c in zip(*matrix))) #oo lala
+        return list(minr&maxc)
