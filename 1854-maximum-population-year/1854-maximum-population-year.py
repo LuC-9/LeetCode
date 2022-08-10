5bfe55454e840fcd2a2490b12e11ad884e7b0bcb
aarsh <68937277+LuC-9@users.noreply.github.com>
2022-08-11 02:06:04 +0530

    Time: 46 ms (90.48%), Space: 13.8 MB (74.83%) - LeetHub

--- 1854-maximum-population-year/1854-maximum-population-year.py
+++ 1854-maximum-population-year/1854-maximum-population-year.py
@@ -1,15 +1,17 @@
 class Solution:
     def maximumPopulation(self, logs: List[List[int]]) -> int:   
-        d = defaultdict(int)
-        res = [2051, 0]
-        for YOB, YOD in logs:
-            for year in range(YOB, YOD):
-                d[year] += 1
-                if d[year] >= res[1]:
-                    if d[year] > res[1]:
-                        res = [year, d[year]]
-                    else:
-                        res = [min(year, res[0]), res[1]]
-        #print(d)
-        #print(res)
-        return res[0]
+        dates = []
+        for birth, death in logs:
+            dates.append((birth, 1))
+            dates.append((death, -1))
+            
+        dates.sort()
+
+        population = max_population = max_year = 0
+        for year, change in dates:
+            population += change
+            if population > max_population:
+                max_population = population
+                max_year = year
+        
+        return max_year
