e3a7366bf5cd4b107390b323d612ee5f17ff0f92
aarsh <68937277+LuC-9@users.noreply.github.com>
2022-08-11 02:19:42 +0530

    Time: 33 ms (99.51%), Space: 13.8 MB (74.83%) - LeetHub

--- 1854-maximum-population-year/1854-maximum-population-year.py
+++ 1854-maximum-population-year/1854-maximum-population-year.py
@@ -1,14 +1,18 @@
 class Solution:
     def maximumPopulation(self, logs: List[List[int]]) -> int:   
-        birth_years = sorted([i[0] for i in logs])
-        max_pop = 0
-        output = 0
-        for year in birth_years:
-            population= 0
-            for dates in logs:
-                if dates[0]<=year and dates[1]>year:
-                    population+=1
-            if population>max_pop:
-                output = year
-                max_pop = population
-        return output
+        dates = []
+        for birth, death in logs:
+            dates.append((birth, 1))
+            dates.append((death, -1))
+        # har birth pe +1 and hr death pe -1
+        dates.sort()
+        #sort kr lo
+        #ab maximum running sum kahan aa rha nikal lo
+        population = max_population = max_year = 0
+        for year, change in dates:
+            population += change
+            if population > max_population: #change tbhi krna h jb jyada ho qki same hone pe chhota wala year return krna h
+                max_population = population
+                max_year = year
+        
+        return max_year
