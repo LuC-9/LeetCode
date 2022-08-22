f28fcc2405cd8789bee3e1efd4ee03151608e327
aarsh <68937277+LuC-9@users.noreply.github.com>
2022-08-23 03:01:26 +0530

    Time: 119 ms (91.33%), Space: 15.2 MB (57.00%) - LeetHub

--- 885-spiral-matrix-iii/885-spiral-matrix-iii.py
+++ 885-spiral-matrix-iii/885-spiral-matrix-iii.py
@@ -1,14 +1,32 @@
 class Solution:
     def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
         directions=[(0,1),(1,0),(0,-1),(-1,0)]
+        #East,South,West,North
         dir_idx=0
+        steps=1
         inc=1
         res=[[rStart,cStart]]
         while(len(res)<rows*cols):
             for i in range(inc):
                 rStart,cStart=rStart+directions[dir_idx][0],cStart+directions[dir_idx][1]
+                # increase according to directions 
+                
+                # if it goes out of matrix discard or else append in result
                 if rStart>=0 and rStart<rows and cStart>=0 and cStart<cols:
+                    
                     res.append([rStart,cStart])
             dir_idx=(dir_idx+1)%4
-            inc+= not(dir_idx%2)
+            # change the direction after suitable steps
+            
+            # if current steps(steps) are even increment the inc(i.e steps to be taken)
+            if steps%2==0:
+                inc+=1
+            steps+=1
+            """
+            
+            we can avoid use of variable step by using below snippet in it's place
+            inc += not(dir_idx%2), i.e. add 1 if dir_idx is even or 0
+            
+            """
+            
         return res
