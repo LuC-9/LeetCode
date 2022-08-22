5bc8f7e1b519b3d58829286b0ff75dd685ae0187
aarsh <68937277+LuC-9@users.noreply.github.com>
2022-08-23 02:48:14 +0530

    Time: 163 ms (63.33%), Space: 15.3 MB (57.00%) - LeetHub

--- 885-spiral-matrix-iii/885-spiral-matrix-iii.py
+++ 885-spiral-matrix-iii/885-spiral-matrix-iii.py
@@ -1,20 +1,17 @@
 class Solution:
     def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
         directions=[(0,1),(1,0),(0,-1),(-1,0)]
-        #            E       S      W      N
         dir_idx=0
-        tot=rows*cols
         steps=1
         inc=1
         res=[[rStart,cStart]]
-        while(len(res)<tot):
-            for i in range (inc):
+        while(len(res)<rows*cols):
+            for i in range(inc):
                 rStart,cStart=rStart+directions[dir_idx][0],cStart+directions[dir_idx][1]
-                if rStart>=0 and rStart<rows and cStart >= 0 and cStart < cols:
+                if rStart>=0 and rStart<rows and cStart>=0 and cStart<cols:
                     res.append([rStart,cStart])
             dir_idx=(dir_idx+1)%4
-            if steps % 2 ==0:
+            if steps%2==0:
                 inc+=1
             steps+=1
         return res
-        
