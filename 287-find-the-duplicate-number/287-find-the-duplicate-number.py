d6f59be88f74d61ad66b2bf2dc9b39bcb6b99d3c
aarsh <68937277+LuC-9@users.noreply.github.com>
2022-08-31 04:04:43 +0530

    Time: 1253 ms (19.63%), Space: 27.8 MB (98.18%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -3,9 +3,9 @@ class Solution:
         for i in array:
             absi=abs(i)
             # if the mapped index value is already negative
-            if array[absi%len(array)]<0:
+            if array[absi-1]<0:
                 return absi
             #make the mapped value negative(a value is mapped to index element%len(array)
-            array[absi%len(array)]*=-1
+            array[absi-1]*=-1
         return -1
         
