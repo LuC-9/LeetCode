139d5781cb8b8abc4e56dac2e6c1fdd0aeb12e65
aarsh <68937277+LuC-9@users.noreply.github.com>
2022-09-07 00:49:26 +0530

    Time: 65 ms (19.80%), Space: 14 MB (14.21%) - LeetHub

--- 27-remove-element/27-remove-element.py
+++ 27-remove-element/27-remove-element.py
@@ -1,8 +1,7 @@
 class Solution:
     def removeElement(self,array: List[int], val: int) -> int:
         i = 0
-        for index in range(len(array)):
-            
+        for index in range(len(array)):    
             if array[index] != val:
                 array[index], array[i] = array[i], array[index]
                 i += 1
