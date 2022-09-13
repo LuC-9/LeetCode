ee497500ae5785118ab421a0510354f542f10888
aarsh <68937277+LuC-9@users.noreply.github.com>
2022-09-13 23:21:25 +0530

    Time: 226 ms (24.14%), Space: 13.6 MB (72.41%) - LeetHub

---
+++ 393-utf-8-validation/393-utf-8-validation.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def validUtf8(self, data):
+        """
+        :type data: List[int]
+        :rtype: bool
+        """
+        cnt = 0 
+        for x in data:
+            x = bin(x)[2:].zfill(8)
+            if cnt: # in the middle of multi-byte 
+                if x.startswith("10"): cnt -= 1
+                else: return False 
+            else: # beginning 
+                cnt = x.find("0")
+                if cnt == -1 or cnt == 1 or cnt > 4: return False 
+                if cnt: cnt -= 1
+        return cnt == 0
+        
