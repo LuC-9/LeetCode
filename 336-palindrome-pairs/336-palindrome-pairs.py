0294b933d406bfe5ff1a346c1e1be26ebddde0d8
aarsh <68937277+LuC-9@users.noreply.github.com>
2022-09-18 01:09:21 +0530

    Time: 6210 ms (27.10%), Space: 24.9 MB (99.59%) - LeetHub

--- 336-palindrome-pairs/336-palindrome-pairs.py
+++ 336-palindrome-pairs/336-palindrome-pairs.py
@@ -1,17 +1,19 @@
-class Solution(object):
-    def palindromePairs(self, words):
-        """
-        :type words: List[str]
-        :rtype: List[List[int]]
-        """
-        lookup = {wrd[::-1]: idx for idx, wrd in enumerate(words)}
+class Solution:
+    def palindromePairs(self, words: List[str]) -> List[List[int]]:
         res = []
-        for idx, wrd in enumerate(words):
-			for j in range(len(wrd)+1):
-				pre, pos = wrd[:j], wrd[j:]
-				if not len(pre) == 0 and pre == pre[::-1] and pos in lookup and lookup[pos] != idx:
-					res.append([lookup[pos], idx])
-				if pos == pos[::-1] and pre in lookup and lookup[pre] != idx:
-					res.append([idx, lookup[pre]])
+        lookup = {}
+        for i, word in enumerate(words):
+            lookup[word] = i
+
+        for i in range(len(words)):
+            for j in range(len(words[i]) + 1):
+                prefix = words[i][j:]
+                suffix = words[i][:j]
+                if prefix == prefix[::-1] and \
+                   suffix[::-1] in lookup and lookup[suffix[::-1]] != i:
+                    res.append([i, lookup[suffix[::-1]]])
+                if j > 0 and suffix == suffix[::-1] and \
+                   prefix[::-1] in lookup and lookup[prefix[::-1]] != i:
+                    res.append([lookup[prefix[::-1]], i])
         return res
         
