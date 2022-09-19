b1e96e5344fb2975e26999561e2ab9fcead5f9da
aarsh <68937277+LuC-9@users.noreply.github.com>
2022-09-20 01:31:27 +0530

    Time: 183 ms (22.06%), Space: 29.3 MB (85.29%) - LeetHub

--- 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.py
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.py
@@ -4,37 +4,12 @@ class Solution(object):
         :type paths: List[str]
         :rtype: List[List[str]]
         """
-        # this regex has two group: 
-		# 1. a file name group 
-		# 2. a content group
-		#
-		# Ex: "root/a 1.txt(abcd) 2.txt(efgh)"
-		# 
-		# Output: [('1.txt', 'abcd'), ('2.txt', 'efgh')]
-        content_regex = re.compile("(\w+.txt)\((.*?)\)", re.IGNORECASE)
-		
-		# hashmap to keep everything
-		#
-        d = defaultdict(list)
-		
-		# go through each path
-		# 
-        for path in paths:
-			
-			# a regex expression is not needed as the dir path
-			# is always the first item and is seperated by a space
-			#
-            root = path.split(" ")[0]
-            file = re.findall(content_regex, path)
-            
-            for file_name, content in file:
-                
-				# store a list of file plus its directory under the content of each file
-				#
-                d[content].append(root + "/" + file_name)            
-        
-		# filtering out anything that has a length that is smaller than 1
-		# since it wouldn't be a group
-		#
-        return list(filter(lambda x: len(x) > 1, d.values())) 
+        dic=defaultdict(lambda :[])
+        for i in range(len(paths)):
+            st=paths[i]
+            lst=st.split(" ")
+            for j in range(1,len(lst)):
+                sp=lst[j].index("(")
+                dic[lst[j][sp:]].append(lst[0]+"/"+lst[j][:sp])
+        return [dic[i] for i in dic if len(dic[i])>1]
         
