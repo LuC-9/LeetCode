586721a856cece3bdbb50afe02ae8f7f9183f902
aarsh <68937277+LuC-9@users.noreply.github.com>
2022-09-20 02:40:17 +0530

    Time: 56 ms (99.09%), Space: 15.2 MB (50.11%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,6 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen = {}
+
         for index, num in enumerate(nums):
             other = target - num 
 
